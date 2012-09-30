a3ee7c1dabaa9a413b5b96dc1b3cf7484a8c9226
Jeff Lawson <jeff.lawson@flightaware.com>
2012-09-30 02:13:11 -0500

    stub out command handlers for connection and context objects

--- generic/tclwebsockets.c
+++ generic/tclwebsockets.c
@@ -15,19 +15,121 @@
 #define TCL_STORAGE_CLASS DLLEXPORT
 
 
-struct websocket_handler_definition {
-  char *handler_name;
-  char *sessionvars[];
-  
-}
 
 
 struct websocket_session_struct {
   const char *handler_name;
-  const char *session_array_name;
+  char session_array_name[64];
+  char connection_cmd_name[64];
+  Tcl_Interp *interp;
+
+  Tcl_Obj *queued_data;
 };
 
 
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * tclwebsockets_connectionCmd --
+ *
+ *    Allows the caller to invoke various commands against a client connection.
+ *
+ * Results:
+ *    stuff
+ *
+ *----------------------------------------------------------------------
+ */
+int
+tclwebsockets_connectionCmd(ClientData cData, Tcl_Interp *interp, int objc, Tcl_Obj *CONST objv[])
+{
+  // cData points to websocket_session_struct?
+
+  const char *commands[] = {
+    "close",
+    "write",
+    NULL
+  };
+
+  enum command_enum {
+    CMD_CLOSE,
+    CMD_WRITE
+  };
+
+  switch (cmd) {
+  case CMD_CLOSE: {
+    libwebsocket_close_and_free_session(context, wsi,
+					LWS_CLOSE_STATUS_NORMAL);
+
+  }
+  case CMD_WRITE: {
+    n = libwebsocket_write(wsi, p, n, LWS_WRITE_TEXT);
+    if (n < 0) {
+      fprintf(stderr, "ERROR writing to socket");
+      return 1;
+    }
+    // otherwise add to queued_data
+  }
+  }
+
+
+}
+
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * tclwebsockets_contextCmd --
+ *
+ *    Allows the caller to invoke various commands against a server context.
+ *
+ * Results:
+ *    stuff
+ *
+ *----------------------------------------------------------------------
+ */
+int
+tclwebsockets_contextCmd(ClientData cData, Tcl_Interp *interp, int objc, Tcl_Obj *CONST objv[])
+{
+  // cData points to the context?
+
+  const char *commands[] = {
+    "service",
+    "delete",
+    NULL
+  };
+
+  enum command_enum {
+    CMD_SERVICE,
+    CMD_DELETE
+  };
+
+  switch (cmd) {
+  case CMD_SERVICE: {
+    int n = libwebsocket_service(context, 50);
+  }
+  case CMD_DELETE: {
+    libwebsocket_context_destroy(context);
+    // delete the Tcl command
+    // ckfree the memory for the protocol array.
+  }
+
+}
+
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * callback_websocket_handler --
+ *
+ *    Universal callback invoked by libwebsocket whenever an event
+ *    occurs on a connection belonging to any of our protocols.
+ *
+ * Results:
+ *    stuff
+ *
+ *----------------------------------------------------------------------
+ */
 static int
 callback_websocket_handler(struct libwebsocket_context *context,
 		    struct libwebsocket *wsi,
@@ -36,36 +138,75 @@ callback_websocket_handler(struct libwebsocket_context *context,
 {
   int n;
   struct websocket_session_struct *session_data = (struct websocket_session_struct *)user;
+  Tcl_Interp *interp = NULL;
+  Tcl_Obj *handlerRegistryList = NULL;
 
+
+  // When a connection is first established, do some extra work to
+  // intitialize the session structure.
   if (reason == LWS_CALLBACK_ESTABLISHED) {
     const struct libwebsocket_protocols *protocol;
 
     // initialize session_data
     protocol = libwebsockets_get_protocol(wsi);
-    context->handler_name = protocol->name;
+    session_data->handler_name = protocol->name;
 
     // generate a unique session_array_name.
+    // generate a unique connection_command_name.
     #error TODO
+
+    // find the original Tcl interp.
+    #error TODO
+    session_data->queued_data = NULL;
+
+    // register a new command in the Tcl interpreter to represent this connection
+    // using connection_command_name and tclwebsockets_connectionCmd
+  }
+
+
+  interp = session_data->interp;
+
+
+  // Look up the definition from our registry array.
+  handlerRegistryList = Tcl_GetVar2Ex(interp, "::websockets::handlerRegistry", context->handler_name, TCL_GLOBAL_ONLY);
+  if (handlerRegistryList == NULL) {
+    fprintf(stderr, "callback_websocket_handler called for an unknown protocol \"%s\"\n", context->handler_name);
+    return -1;
   }
 
 
   switch (reason) {
   case LWS_CALLBACK_ESTABLISHED: {
+    "established"
   }
 
   case LWS_CALLBACK_BROADCAST: {
+    "broadcast"
   }
 
   case LWS_CALLBACK_FILTER_NETWORK_CONNECTION: {
+    //"filter-network"
   }
   case LWS_CALLBACK_FILTER_PROTOCOL_CONNECTION: {
+    "filter-protocol"
   }
   case LWS_CALLBACK_HTTP: {
+    "http"
   }
   case LWS_CALLBACK_RECEIVE: {
+    "receive"
   }
   case LWS_CALLBACK_SERVER_WRITEABLE: {
+    if (session_data->queued_data != NULL) {
+      ...
+    }
   }
+
+  case LWS_CALLBACK_CLOSED: {
+    // delete the Tcl command for this connection.
+    // unset the session array.
+  }
+
   }
 
     
@@ -331,41 +472,7 @@ tclwebsockets_listenCmd(ClientData cData, Tcl_Interp *interp, int objc, Tcl_Obj 
 
 
 
-/*
- *----------------------------------------------------------------------
- *
- * tclwebsockets_writeCmd --
- *
- *    Allows the caller to queue data for transmission to the specified websocket.
- *
- * Results:
- *    stuff
- *
- *----------------------------------------------------------------------
- */
-int
-tclwebsockets_writeCmd(ClientData cData, Tcl_Interp *interp, int objc, Tcl_Obj *CONST objv[])
-{
-}
 
-
-/*
- *----------------------------------------------------------------------
- *
- * tclwebsockets_closeCmd --
- *
- *    Allows the caller to close a websocket.
- *
- * Results:
- *    stuff
- *
- *----------------------------------------------------------------------
- */
-int
-tclwebsockets_closeCmd(ClientData cData, Tcl_Interp *interp, int objc, Tcl_Obj *CONST objv[])
-{
-
-}
 
 
 
@@ -391,14 +498,13 @@ EXTERN int
 Tclwebsockets_Init(Tcl_Interp *interp)
 {
     /*
-     * This may work with 8.0, but we are using strictly stubs here,
-     * which requires 8.1.
+     * This may work with older versions of Tcl, but I've only tested against Tcl 8.5.
      */
-    if (Tcl_InitStubs(interp, "8.1", 0) == NULL) {
+    if (Tcl_InitStubs(interp, "8.5", 0) == NULL) {
 	return TCL_ERROR;
     }
 
-    if (Tcl_PkgRequire(interp, "Tcl", "8.1", 0) == NULL) {
+    if (Tcl_PkgRequire(interp, "Tcl", "8.5", 0) == NULL) {
 	return TCL_ERROR;
     }
 
@@ -411,3 +517,4 @@ Tclwebsockets_Init(Tcl_Interp *interp)
 
     return TCL_OK;
 }
+
